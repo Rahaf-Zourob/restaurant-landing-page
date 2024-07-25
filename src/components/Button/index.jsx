938cb221345a7d3ff1f5d2b8f7cf4fac35ddcf6b
RahafZourob <zourobrahaf@gmail.com>
2024-07-25 03:51:22 +0300

    Use style instead styles

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,6 +1,6 @@
-import style from "./button.module.css"
+import styles from "./button.module.css"
 export default function Button({ shape, rate, handelClick, children }) {
     return (
-        <button onClick={handelClick} className={`${style.button}  ${shape === "circle" ? style.circle : style.rectangle}  ${rate && style.btn_rate}`}>{children}</button>
+        <button onClick={handelClick} className={`${styles.button}  ${shape === "circle" ? styles.circle : styles.rectangle}  ${rate && styles.btn_rate}`}>{children}</button>
     )
 }
