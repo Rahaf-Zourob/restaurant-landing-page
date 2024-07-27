6ef970ca2cbd821b30d11d807a883df955fc75ba
RahafZourob <zourobrahaf@gmail.com>
2024-07-27 04:41:03 +0300

    Add Menu Data

---
+++ src/mock/fondasData.js
@@ -0,0 +1,34 @@
+export const fondasData = [
+    {
+        id: 1,
+        rate: 9.8,
+        name: "Doña Laura",
+        time: "20-30 min",
+        classify:"Fonditas",
+        image:"/assets/images/donaLaura.png"
+    },
+    {
+        id: 2,
+        rate: 8.5,
+        name: "Rosa Cafe",
+        time: "~45 min",
+        classify:"Lonchería",
+        image:"/assets/images/rosaCafe.png"
+    },
+    {
+        id: 3,
+        rate: 7.3,
+        name: "Le cottidiene",
+        time: "15-20 min",
+        classify:"Sushi",
+        image:"/assets/images/leCottidiene.png"
+    },
+    {
+        id: 4,
+        rate: 9.8,
+        name: "Querreque",
+        time: "~50 min",
+        classify:"Veggies",
+        image:"/assets/images/querreque.png"
+    },
+]
