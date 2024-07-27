49a4f4a6dbfd98b72ad0bd76f56334565a55ba4c
RahafZourob <zourobrahaf@gmail.com>
2024-07-27 04:24:32 +0300

    Edit button background

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,6 +1,6 @@
 import styles from "./button.module.css"
-export default function Button({ shape, rate, handelClick, children }) {
+export default function Button({ shape, handelClick, children, bgColor }) {
     return (
-        <button onClick={handelClick} className={`${styles.button}  ${shape === "circle" ? styles.circle : styles.rectangle}  ${rate && styles.btn_rate}`}>{children}</button>
+        <button onClick={handelClick} className={`${styles.button}  ${shape === "circle" ? styles.circle : styles.rectangle} ${bgColor ? styles.bgPink : styles.transparent}`}>{children}</button>
     )
 }
