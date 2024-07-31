44525ace5fba757a284247162e7909f519da2363
RahafZourob <zourobrahaf@gmail.com>
2024-07-31 11:14:46 +0300

    edit: add another conditions for list

--- src/components/LinkList/index.jsx
+++ src/components/LinkList/index.jsx
@@ -1,19 +1,30 @@
-import styles from "./linkList.module.css"
+import styles from "./linkList.module.css";
 export default function LinkList({ data, social_media, img, direction }) {
   return (
     <>
-      {img ?
+      {img ? (
         <ul className={styles.img_list}>
-          {
-            data.map(item => <li key={item.id} className={social_media ? styles.social_media : styles.payment}>
+          {data.map((item) => (
+            <li
+              key={item.id}
+              className={social_media ? styles.social_media : styles.payment}
+            >
               <a href={item.link}>{item.svg}</a>
             </li>
-            )
-          } </ul> :
-        <ul className={`${styles.service} ${direction === "row" ? styles.row : styles.column}`}>
-          {data.map(service => <li key={service.id}><a href={service.link}>{service.name}</a></li>)}
+          ))}
         </ul>
-      }
+      ) : (
+        <ul
+          className={`${styles.service} ${direction === "row" ? styles.row : styles.column
+            }`}
+        >
+          {data.map((service) => (
+            <li key={service.id}>
+              <a href={service.link}>{service.name}</a>
+            </li>
+          ))}
+        </ul>
+      )}
     </>
-  )
+  );
 }
